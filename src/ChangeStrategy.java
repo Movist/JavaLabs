4f67068ab95f78f6d49ec828d63c3216f8de2278
Maksim Abaydulin <shtigun@yandex.ru>
2022-09-12 18:25:52 +0300

    Modified class ChangeStrategy

--- src/ChangeStrategy.java
+++ src/ChangeStrategy.java
@@ -1,25 +1,27 @@
 public class ChangeStrategy {
     public void change(int strategy, String text) {
+
         Changeable firstStrategy = new FirstStrategy();
+        Changeable secondStrategy = new SecondStrategy();
+        Changeable thirdStrategy = new ThirdStrategy();
+        Changeable fourStrategy = new FourStrategy();
+        Changeable fiveStrategy = new FiveStrategy();
+
         switch (strategy) {
             case 1 -> {
-                firstStrategy = new FirstStrategy();
                 firstStrategy.changeStrategy(text);
             }
             case 2 -> {
-                firstStrategy = new SecondStrategy();
+                secondStrategy.changeStrategy(text);
 
             }
             case 3 -> {
-                ThirdStrategy thirdStrategy = new ThirdStrategy();
                 thirdStrategy.changeStrategy(text);
             }
             case 4 -> {
-                FourStrategy fourStrategy = new FourStrategy();
                 fourStrategy.changeStrategy(text);
             }
             case 5 -> {
-                FiveStrategy fiveStrategy = new FiveStrategy();
                 fiveStrategy.changeStrategy(text);
             }
             default -> {
